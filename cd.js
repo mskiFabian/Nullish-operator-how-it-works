8b4c13c1f52bc80fe2de286eaa1916a061be9376
Fabian <125907927+mskiFabian@users.noreply.github.com>
2023-04-02 18:15:58 +0200

    Add files via upload

---
+++ cd.js
@@ -0,0 +1,18 @@
+const rest1 = {
+  nameOfRestaurant: 'Capri',
+  numOfGuests: 0
+}
+
+const rest2 = {
+  nameOfRestaurant: 'Pizziana',
+  owner: 'Giovanni Rossi'
+}
+
+rest1.numOfGuests ??= 10
+rest2.numOfGuests ??= 10
+
+console.log(rest1)
+console.log(rest2)
+
+
+
